51e3de97ebdd09a3486b9d129bf7245e73d9e041
WrightEthan <wrightethan321@gmail.com>
2020-02-17 19:04:45 -0500

    bunch of stuff

---
+++ JS/expand.js
@@ -0,0 +1,112 @@
+var expand = document.querySelector('#expandButton');
+var menuOpen1 = document.querySelector('#menuOpen1');
+var menuOpen2 = document.querySelector('#menuOpen2');
+var menuOpen3 = document.querySelector('#menuOpen3');
+var menuOpen4 = document.querySelector('#menuOpen4');
+var menuOpen5 = document.querySelector('#menuOpen5');
+expandButton.addEventListener('click', () => {
+    navbar.classList.toggle("navToggle");
+    navbar.classList.toggle("navToggleD");
+    menuArrow.classList.toggle("closed");
+    menuArrow.classList.toggle("open");
+    menuOpen1.classList.toggle("menuItem");
+    menuOpen1.classList.toggle("menuItemExpanded");
+    menuOpen2.classList.toggle("menuItem");
+    menuOpen2.classList.toggle("menuItemExpanded");
+    menuOpen3.classList.toggle("menuItem");
+    menuOpen3.classList.toggle("menuItemExpanded");
+    menuOpen4.classList.toggle("menuItem");
+    menuOpen4.classList.toggle("menuItemExpanded");
+    menuOpen5.classList.toggle("menuItem");
+    menuOpen5.classList.toggle("menuItemExpanded");
+    // menuOpen1.classList.toggle("menuItem");
+    // menuOpen1.classList.toggle("menuItemExpanded");
+    // menuOpen2.classList.toggle("menuItem");
+    // menuOpen2.classList.toggle("menuItemExpanded");
+    // menuOpen3.classList.toggle("menuItem");
+    // menuOpen3.classList.toggle("menuItemExpanded");
+    // menuOpen4.classList.toggle("menuItem");
+    // menuOpen4.classList.toggle("menuItemExpanded");
+    // menuOpen5.classList.toggle("menuItem");
+    // menuOpen5.classList.toggle("menuItemExpanded");
+})
+menuOpen1.addEventListener('click', () => {
+    navbar.classList.toggle("navToggle");
+    navbar.classList.toggle("navToggleD");
+    menuArrow.classList.toggle("closed");
+    menuArrow.classList.toggle("open");
+    menuOpen1.classList.toggle("menuItem");
+    menuOpen1.classList.toggle("menuItemExpanded");
+    menuOpen2.classList.toggle("menuItem");
+    menuOpen2.classList.toggle("menuItemExpanded");
+    menuOpen3.classList.toggle("menuItem");
+    menuOpen3.classList.toggle("menuItemExpanded");
+    menuOpen4.classList.toggle("menuItem");
+    menuOpen4.classList.toggle("menuItemExpanded");
+    menuOpen5.classList.toggle("menuItem");
+    menuOpen5.classList.toggle("menuItemExpanded");
+})
+menuOpen2.addEventListener('click', () => {
+    navbar.classList.toggle("navToggle");
+    navbar.classList.toggle("navToggleD");
+    menuArrow.classList.toggle("closed");
+    menuArrow.classList.toggle("open");
+    menuOpen1.classList.toggle("menuItem");
+    menuOpen1.classList.toggle("menuItemExpanded");
+    menuOpen2.classList.toggle("menuItem");
+    menuOpen2.classList.toggle("menuItemExpanded");
+    menuOpen3.classList.toggle("menuItem");
+    menuOpen3.classList.toggle("menuItemExpanded");
+    menuOpen4.classList.toggle("menuItem");
+    menuOpen4.classList.toggle("menuItemExpanded");
+    menuOpen5.classList.toggle("menuItem");
+    menuOpen5.classList.toggle("menuItemExpanded");
+})
+menuOpen3.addEventListener('click', () => {
+    navbar.classList.toggle("navToggle");
+    navbar.classList.toggle("navToggleD");
+    menuArrow.classList.toggle("closed");
+    menuArrow.classList.toggle("open");
+    menuOpen1.classList.toggle("menuItem");
+    menuOpen1.classList.toggle("menuItemExpanded");
+    menuOpen2.classList.toggle("menuItem");
+    menuOpen2.classList.toggle("menuItemExpanded");
+    menuOpen3.classList.toggle("menuItem");
+    menuOpen3.classList.toggle("menuItemExpanded");
+    menuOpen4.classList.toggle("menuItem");
+    menuOpen4.classList.toggle("menuItemExpanded");
+    menuOpen5.classList.toggle("menuItem");
+    menuOpen5.classList.toggle("menuItemExpanded");
+})
+menuOpen4.addEventListener('click', () => {
+    navbar.classList.toggle("navToggle");
+    navbar.classList.toggle("navToggleD");
+    menuArrow.classList.toggle("closed");
+    menuArrow.classList.toggle("open");
+    menuOpen1.classList.toggle("menuItem");
+    menuOpen1.classList.toggle("menuItemExpanded");
+    menuOpen2.classList.toggle("menuItem");
+    menuOpen2.classList.toggle("menuItemExpanded");
+    menuOpen3.classList.toggle("menuItem");
+    menuOpen3.classList.toggle("menuItemExpanded");
+    menuOpen4.classList.toggle("menuItem");
+    menuOpen4.classList.toggle("menuItemExpanded");
+    menuOpen5.classList.toggle("menuItem");
+    menuOpen5.classList.toggle("menuItemExpanded");
+})
+menuOpen5.addEventListener('click', () => {
+    navbar.classList.toggle("navToggle");
+    navbar.classList.toggle("navToggleD");
+    menuArrow.classList.toggle("closed");
+    menuArrow.classList.toggle("open");
+    menuOpen1.classList.toggle("menuItem");
+    menuOpen1.classList.toggle("menuItemExpanded");
+    menuOpen2.classList.toggle("menuItem");
+    menuOpen2.classList.toggle("menuItemExpanded");
+    menuOpen3.classList.toggle("menuItem");
+    menuOpen3.classList.toggle("menuItemExpanded");
+    menuOpen4.classList.toggle("menuItem");
+    menuOpen4.classList.toggle("menuItemExpanded");
+    menuOpen5.classList.toggle("menuItem");
+    menuOpen5.classList.toggle("menuItemExpanded");
+})
