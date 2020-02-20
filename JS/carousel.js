03846eee239c4adc9600fb08bd1d99dba1db252c
WrightEthan <wrightethan321@gmail.com>
2020-02-19 22:21:31 -0500

    time to deploy

--- JS/carousel.js
+++ JS/carousel.js
@@ -12,14 +12,14 @@ nextBtn.addEventListener('click', () => {
     if(counter >= carouselImages.length - 1) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
-    // console.log(counter);
+    console.log(counter);
     carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
 });
 prevBtn.addEventListener('click', () => {
     if(counter <= 0) return;
     carouselSlide.style.transition = "transform 0.4s ease-in-out";
     counter--;
-    // console.log(counter);
+    console.log(counter);
     carouselSlide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
 });
 carouselSlide.addEventListener('transitionend', () => {
